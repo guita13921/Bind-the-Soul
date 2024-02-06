c6c83d119ea36cd942ab85aabd71610f000b4da0
กรมพระยาหมื่นเยาวรักษณ์ <guita13921@gmail.com>
2024-02-06 19:19:25 +0700

    Push aniamtion enemy

--- ProjectGameD/Assets/Script/EnemyAI/EnemyAIPartol.cs
+++ ProjectGameD/Assets/Script/EnemyAI/EnemyAIPartol.cs
@@ -7,7 +7,7 @@ using UnityEngine.AI;
 
 public class EnemyAIPartol : MonoBehaviour
 {
-    GameObject player;
+    [SerializeField] GameObject player;
     NavMeshAgent agent;
     [SerializeField] LayerMask groundLayer, playerLayer;
     Vector3 destPoint;
@@ -15,16 +15,21 @@ public class EnemyAIPartol : MonoBehaviour
     [SerializeField]float range;
     [SerializeField]float sightRange , attackRange;
     bool playerInsight, PlayerInAttackrange;
+    Animator animator;
+    BoxCollider boxCollider;
     // Start is called before the first frame update
     void Start()
     {
+        boxCollider = GetComponentInChildren<BoxCollider>();
         agent = GetComponent<NavMeshAgent>();
         player = GameObject.FindWithTag("Player");
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update(){
         playerInsight = Physics.CheckSphere(transform.position, sightRange, playerLayer);
+        PlayerInAttackrange = Physics.CheckSphere(transform.position, attackRange, playerLayer);
 
         if(!playerInsight && !PlayerInAttackrange) Patrol();
         if(playerInsight && !PlayerInAttackrange) Chase();
@@ -36,7 +41,10 @@ public class EnemyAIPartol : MonoBehaviour
     }
 
     void Attack(){
-
+        if(!animator.GetCurrentAnimatorStateInfo(0).IsName("Attack1h1")){
+            animator.SetTrigger("Attack");
+            agent.SetDestination(transform.position);
+        }
     }
 
     void Patrol(){
@@ -55,6 +63,21 @@ public class EnemyAIPartol : MonoBehaviour
             walkpointSet = true;
         }
 
+    }
+
+    void EnableAttack(){
+        boxCollider.enabled = true;
+    }
+
+    void DisableAttack(){
+        boxCollider.enabled = false;
+    }
+
+    private void OnTriggerEnter(Collider other){
+        var player = other.GetComponent<CapsuleCollider>(); //WTF
+        if(player != null){
+            Debug.Log("HIT");
+        }
     }
 
 }
