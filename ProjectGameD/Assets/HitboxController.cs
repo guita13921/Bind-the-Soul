83e15c4b54ba8b2319e4748c6b1ce4d4f9166dd1
NEZE <boss-NeZE@hotmail.com>
2024-12-26 13:29:57 +0700

    hitbox1

--- ProjectGameD/Assets/HitboxController.cs
+++ ProjectGameD/Assets/HitboxController.cs
@@ -1,27 +1,76 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class HitboxController : MonoBehaviour
 {
+    [Header("Timing Settings")]
     public float enabletime = 0f;
-    private BoxCollider boxCollider;
-    private float timer;
+    public bool loop = false;
 
-    // Start is called before the first frame update
+    [Tooltip("Interval between collider toggles when looping.")]
+    public float looptime = 0f;
+
+    private BoxCollider boxCollider;
+    private float timer = 0f;
+    private float loopTimer = 0f;
+    private bool isColliderEnabled = false;
+    public float destroyTime = 0f;
+
     void Start()
     {
         boxCollider = GetComponent<BoxCollider>();
         boxCollider.enabled = false;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        timer += Time.deltaTime;
-        if (timer >= enabletime)
+        if (loop)
         {
-            boxCollider.enabled = true;
+            HandleLoop();
         }
+        else
+        {
+            HandleSingleActivation();
+        }
+    }
+
+    private void HandleLoop()
+    {
+        timer += Time.deltaTime;
+        loopTimer += Time.deltaTime;
+
+        if (timer >= enabletime && !isColliderEnabled)
+        {
+            ToggleBoxCollider(true);
+        }
+
+        if (loopTimer >= looptime)
+        {
+            ToggleBoxCollider(!boxCollider.enabled);
+            loopTimer = 0f;
+        }
+
+        if (timer >= destroyTime)
+            Destroy(gameObject);
+    }
+
+    private void HandleSingleActivation()
+    {
+        timer += Time.deltaTime;
+
+        if (timer >= enabletime && !isColliderEnabled)
+        {
+            ToggleBoxCollider(true);
+        }
+
+        if (isColliderEnabled && timer >= enabletime + 0.1f)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void ToggleBoxCollider(bool state)
+    {
+        boxCollider.enabled = state;
+        isColliderEnabled = state;
     }
 }
