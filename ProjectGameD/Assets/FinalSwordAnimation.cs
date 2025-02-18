ef59129110d7b3d91e3f1c2c363381076aa870e4
NEZE <boss-NeZE@hotmail.com>
2025-02-18 19:11:15 +0700

    ending scene add

--- ProjectGameD/Assets/FinalSwordAnimation.cs
+++ ProjectGameD/Assets/FinalSwordAnimation.cs
@@ -4,44 +4,47 @@ using UnityEngine;
 
 public class FinalSwordAnimation : MonoBehaviour
 {
-public Animator animator;
+    public Animator animator;
     public GameObject sword;
     public GameObject player;
     bool startmove = false;
     public float moveSpeed = 2;
-
+    public AudioSource audioSource;
     public Animator cam;
+    public AudioSource run;
+    public AudioSource lightS;
+    public float fadeDuration = 3f; // Time to fade out sound
 
-    void Start()
+    void Start() { }
+
+    public void Swrod()
     {
+        //sword.transform.position = new Vector3( sword.transform.position.x, 1.926469f,  sword.transform.position.z);
+        //sword.transform.rotation = Quaternion.Euler(98.22501f, 18.675f, 117.289f);
+        Rigidbody rb = sword.GetComponent<Rigidbody>();
 
-
-
-}
-    public  void Swrod()
-    {
-             //sword.transform.position = new Vector3( sword.transform.position.x, 1.926469f,  sword.transform.position.z);
-               //sword.transform.rotation = Quaternion.Euler(98.22501f, 18.675f, 117.289f);
-                               Rigidbody rb = sword.GetComponent<Rigidbody>();
-
-            rb.constraints = RigidbodyConstraints.None; // Unfreeze all constraints
+        rb.constraints = RigidbodyConstraints.None; // Unfreeze all constraints
         Collider col = sword.GetComponent<Collider>();
-            col.enabled = true; // Activate collider
+        col.enabled = true; // Activate collider
+
+        audioSource.Play();
     }
 
-    public void Run(){
-                 //   animator.Play("FinalRun");
-
+    public void Run()
+    {
+        //   animator.Play("FinalRun");
     }
 
-        
-
-        public void Run2(){
-            animator.Play("FinalRun2");
-startmove = true;
+    public void Run2()
+    {
+        animator.Play("FinalRun2");
+        startmove = true;
+        run.Play();
+        lightS.Play();
+        StartCoroutine(FadeOutRunSound());
     }
 
-   void Update()
+    void Update()
     {
         if (startmove && player != null)
         {
@@ -49,5 +52,17 @@ startmove = true;
         }
     }
 
+    private IEnumerator FadeOutRunSound()
+    {
+        float startVolume = run.volume;
 
+        for (float t = 0; t < fadeDuration; t += Time.deltaTime)
+        {
+            run.volume = Mathf.Lerp(startVolume, 0, t / fadeDuration);
+            yield return null;
+        }
+
+        run.volume = 0;
+        run.Stop();
+    }
 }
